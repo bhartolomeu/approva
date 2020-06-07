69b60a5bd1cc3e13089c1306d9b478543280587a
bhartolomeu <40629928+bhartolomeu@users.noreply.github.com>
2020-06-07 18:44:20 -0300

    Início do projeto
    Os arquivos identificados devem ser melhorados, ainda há erros na homepage.
    Link de onde a foto foi conseguida(adquirida de de forma gratuita): https://www.pexels.com/pt-br/foto/alimento-alvorecer-amanhecer-arvore-5834/

---
+++ codigo.js
@@ -0,0 +1,9 @@
+function mudar_cor(){
+	var msg = window.document.getElementById('msg_comeco')
+	document.body.style.color = '#abf'
+}
+
+function voltar_cor(){
+	var msg = window.document.getElementById('msg_comeco')
+	document.body.style.color = '#000'
+}
