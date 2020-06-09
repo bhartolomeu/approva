bc3243dc540aec790f077d9e10e63365ad8a9b7b
root <bhartolomeu1@hmail.com>
2020-06-09 09:29:30 -0300

    As páginas agora estão com um visual mais bonito                      As páginas estão com um visual mais apresentável. Na página de login há um lnk para a de cadastro e vice versa

--- codigo.js
+++ codigo.js
@@ -1,7 +1,17 @@
 function ir_para_pagina(url_pagina){
-	botao = document.getElementById('log')
 	window.location.href=url_pagina
 }
-function cadastrando(){
-	console.log('Botão cadastro funciona')
+function novo_usuario(){
+	    usuario = document.getElementById('usuario').value
+	    senha = document.getElementById('senha').value
+	    nome = document.getElementById('nome').value
+    	email = document.getElementById('email').value
+    	telefone = document.getElementById('telefone').value
+    	//Agora precisa armazenar tudo isso
+    	console.log(nome+' criou o usuário '+usuario+' com a senha '+senha+' com o e-mail '+email+' e com o telefone'+ telefone)
+}
+function login(){
+	usuario = document.getElementById('usuario').value
+	senha = document.getElementById('senha').value
+	console.log('O usuario '+usuario+' com senha ' +senha+" está pronto para logar")
 }
