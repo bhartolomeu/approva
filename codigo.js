d1d8c317908d2450866a519fc99efad0459962c2
root <bhartolomeu1@hmail.com>
2020-06-10 19:37:49 -0300

    Página login quase pronta para autenticar                             A página login agora faz autenticação, mas só com usuário "alguem" com senha "algo". A página de cadastro agora encaminha o usuario para a página login após feito o cadastro. O item footer agora também está um pouco mais bonito. Embora o cadastro não possa ser armazenado, ele pode capturar as informações inseridas nos campos de input.

--- codigo.js
+++ codigo.js
@@ -1,18 +1,30 @@
  function ir_para_pagina(url_pagina){
 	window.location.href=url_pagina
 }
-function novo_usuario(){
-	    usuario = document.getElementById('usuario').value
-	    senha = document.getElementById('senha').value
-	    nome = document.getElementById('nome').value
-    	email = document.getElementById('email').value
-    	telefone = document.getElementById('telefone').value
-    	//Agora precisa armazenar tudo isso
-    	console.log(nome+' criou o usuário '+usuario+' com a senha '+senha+' com o e-mail '+email+' e com o telefone'+ telefone)
-}
+
 function login(){
 	usuario = document.getElementById('usuario').value
 	senha = document.getElementById('senha').value
-	console.log('O usuario '+usuario+' com senha ' +senha+" está pronto para logar")
-	ir_para_pagina('conteudo.html')
+	
+	if(usuario=='alguem' && senha=='algo'){
+		console.log('O usuario '+usuario+' com senha ' +senha+" está pronto para logar")
+    	ir_para_pagina('conteudo.html')
+    }else{
+    	console.log('O usuario '+usuario+' com senha ' +senha+" não está cadastrado")
+    	window.alert('USUÁRIO NÃO CADASTRADO')
+    }
+
+}
+
+function Usuario(){ //Essa é a função construora de usuários
+ 	this.usuario = document.getElementById('usuario').value
+    this.senha = document.getElementById('senha').value
+    this.nome = document.getElementById('nome').value
+   	this.email = document.getElementById('email').value
+   	this.telefone = document.getElementById('telefone').value
+   	//Agora precisa armazenar tudo isso
+   	console.log(this.nome+' criou o usuário '+this.usuario+' com a senha '+this.senha+' com o e-mail '+this.email+' e com o telefone'+ this.telefone)
+   	if(window.confirm("Você está cadastrado, quer fazer login?")){
+   		ir_para_pagina('login.html')
+   	}
 }
