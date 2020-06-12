7b312e82c87bcffd8a95c6c64189250fd00d206f
root <bhartolomeu1@hmail.com>
2020-06-12 14:20:48 -0300

    Retirei o usuario do cadastro e estou implementando um "banco"        O usuário no cadastro não tinha mais utilidade, afinal o sistema é quem cria o id de forma randômica e o insere no "banco de dados". Como só teria uma tabela apenas para colocar no banco de dados, não faz sentido utilizar o mysql para tal, afinal só aumentaria a complexidade e o trabalho por pouco retorno. Ainda há pequenos problemas para acessar e escrever no arquivo texto do banco, mas está enacaminhado.

--- codigo.js
+++ codigo.js
@@ -3,28 +3,54 @@
 }
 
 function login(){
-	usuario = document.getElementById('usuario').value
-	senha = document.getElementById('senha').value
-	
-	if(usuario=='alguem' && senha=='algo'){
-		console.log('O usuario '+usuario+' com senha ' +senha+" está pronto para logar")
+	let email = document.getElementById('email').value
+	let senha = document.getElementById('senha').value
+	if(email=='alguem' && senha=='algo'){
+		console.log('O usuario '+email+' com senha ' +senha+" está pronto para logar")
     	ir_para_pagina('conteudo.html')
     }else{
-    	console.log('O usuario '+usuario+' com senha ' +senha+" não está cadastrado")
-    	window.alert('USUÁRIO NÃO CADASTRADO')
+    	console.log('O usuario '+email+' com senha ' +senha+" não está cadastrado")
+    	window.alert('USUÁRIO INVÁLIDO')
     }
 
 }
 
-function Usuario(){ //Essa é a função construora de usuários
- 	this.usuario = document.getElementById('usuario').value
-    this.senha = document.getElementById('senha').value
-    this.nome = document.getElementById('nome').value
-   	this.email = document.getElementById('email').value
-   	this.telefone = document.getElementById('telefone').value
-   	//Agora precisa armazenar tudo isso
-   	console.log(this.nome+' criou o usuário '+this.usuario+' com a senha '+this.senha+' com o e-mail '+this.email+' e com o telefone'+ this.telefone)
+//Função para abrir o arquivo de registro
+function EscreverNoArquivo(arquivo,novo_usuario){//Realmente precisamos desta função?
+	//Aqui escrevemos o texto no arquivo
+	arquivo.WriteLine(novo_usuario.id_usuario+','+novo_usuario.nome+','+novo_usuario.senha+','+novo_usuario.email+','+novo_usuario.telefone+';')
+	//Aqui fechamos o arquivo
+	arquivo.Close();
+}
+
+function Usuario(senha,nome,email,telefone){ //Essa é a função construora de usuários
+	this.id_usuario = Math.floor(Math.random() * 100000000) //O id deve ser gerado ramdomicamente
+	this.senha = senha
+	this.nome = nome
+	this.email = email
+	this.telefone = telefone
+}
+
+function novo_usuario(){
+	let fs = Server.CreateObject("Scripting.FileSystemObject")
+	let dados = new ActiveXObject(fs);
+	//Modo de captura 1: armazenamos tudo numa variável primeiro e depois construimos o objeto
+	/*Aqui capturamos os valores nos inputs da página de cadastro
+ 	let senha = document.getElementById('senha').value
+    let nome = document.getElementById('nome').value
+   	let email = document.getElementById('email').value
+   	let telefone = document.getElementById('telefone').value
+   	Agora precisa armazenar tudo isso...*/
+
+    //Modo de captura 2: construimos o objeto direto com a leitura dos inputs sem passar por uma variável primeiro
+    var novo_usuario = new Usuario(document.getElementById('senha').value,document.getElementById('nome').value,document.getElementById('email').value,document.getElementById('telefone').value);
+    let arquivo = dados.OpenTextFile('registro_usuarios.txt', 1, true);//Aqui abrimos o arquivo
+  	EscreverNoArquivo(arquivo,novo_usuario)
+	console.log(novo_usuario.nome+' criou uma conta com a senha '+novo_usuario.senha+', com o e-mail '+novo_usuario.email+' e com o telefone'+novo_usuario.telefone)
+   	console.log('O sistema gerou para ele o id:',novo_usuario.id_usuario)
    	if(window.confirm("Você está cadastrado, quer fazer login?")){
    		ir_para_pagina('login.html')
    	}
 }
+
+
