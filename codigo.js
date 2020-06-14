2bbb500f20871067aa653d5e877bd81bfa1a0d56
bhartolomeu <40629928+bhartolomeu@users.noreply.github.com>
2020-06-13 23:40:20 -0300

    Estou perto de conseguir usar o "banco de dados"
    Descobri um comando que consegue abrir o arquivo, ele utiliza o XMLHttpRequest.open() para abrir o registro de usuários. Mas ainda dá erro... O erro ocorre pelo fato de que o arquivo registro_usuarios.txt deveria estar num servidor, caso contrário, o javascript impede de funcionar por motivos de segurança do usuário.

--- codigo.js
+++ codigo.js
@@ -1,56 +1,69 @@
- function ir_para_pagina(url_pagina){
-	window.location.href=url_pagina
-}
 
-function login(){
-	let email = document.getElementById('email').value
-	let senha = document.getElementById('senha').value
-	if(email=='alguem' && senha=='algo'){
-		console.log('O usuario '+email+' com senha ' +senha+" está pronto para logar")
-    	ir_para_pagina('conteudo.html')
-    }else{
-    	console.log('O usuario '+email+' com senha ' +senha+" não está cadastrado")
-    	window.alert('USUÁRIO INVÁLIDO')
+class Usuario{
+    constructor(senha,nome,email,telefone){ //Essa é a função construora de usuários
+	    this.id_usuario = Math.floor(Math.random() * 100000000); //O id deve ser gerado ramdomicamente
+	    this.senha = senha;
+	    this.nome = nome;
+	    this.email = email;
+	    this.telefone = telefone;
     }
 
+    get login(){
+		let email = document.getElementById('email').value;
+		let senha = document.getElementById('senha').value;
+		if(email=='alguem' && senha=='algo'){
+			console.log('O usuario '+email+' com senha ' +senha+" está pronto para logar");
+    		ir_para_pagina('conteudo.html');
+	    }else{
+    		console.log('O usuario '+email+' com senha ' +senha+" não está cadastrado");
+    		window.alert('USUÁRIO INVÁLIDO');
+    	}
+    }
 }
-
-//Função para abrir o arquivo de registro
-function EscreverNoArquivo(arquivo,novo_usuario){//Realmente precisamos desta função?
-	//Aqui escrevemos o texto no arquivo
-	arquivo.WriteLine(novo_usuario.id_usuario+','+novo_usuario.nome+','+novo_usuario.senha+','+novo_usuario.email+','+novo_usuario.telefone+';')
+function cadastro(){
+    //let dados = new XMLHttpRequest();
+    //pegaip01()
+	//pegaip02()
+	let dados = new XMLHttpRequest();
+	let novo_usuario = new Usuario(document.getElementById('senha').value,
+    	document.getElementById('nome').value,
+   		document.getElementById('email').value,
+   		document.getElementById('telefone').value);
+   	let arquivo = dados.open('GET','registro_usuarios.txt', false);//Aqui abrimos o arquivo
+   	//https://github.com/nickewing/line-reader Projeto do github que permite ler arquivos linha a linha
+  	arquivo.WriteLine(novo_usuario.id_usuario+
+		','+novo_usuario.nome+
+		','+novo_usuario.senha+
+		','+novo_usuario.email+
+		','+novo_usuario.telefone+';');
 	//Aqui fechamos o arquivo
 	arquivo.Close();
+	//if(window.confirm("Você está cadastrado, quer fazer login?")){
+	//	ir_para_pagina('login.html');
+	//}
+}
+/*
+function pegaip01(){
+    var script=document.createElement('script');
+    script.src='http://ip-api.com/json/?callback=DADOS_IP&fields=33292287';
+    document.body.appendChild(script);
 }
 
-function Usuario(senha,nome,email,telefone){ //Essa é a função construora de usuários
-	this.id_usuario = Math.floor(Math.random() * 100000000) //O id deve ser gerado ramdomicamente
-	this.senha = senha
-	this.nome = nome
-	this.email = email
-	this.telefone = telefone
+function pegaip02(){
+
+    var url = 'http://ip-api.com/json/?lang=pt-BR&fields=33292287';
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+	    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+	        var dados  = JSON.parse(xmlhttp.response);
+	        DADOS_IP(dados);
+	    }
+    }
+    let texto = xmlhttp.open("GET", 'registro_usuarios.txt');
+    console.log(texto)
+    xmlhttp.send();
+}*/
+
+function ir_para_pagina(url_pagina){
+	window.location.href=url_pagina;
 }
-
-function novo_usuario(){
-	let fs = Server.CreateObject("Scripting.FileSystemObject")
-	let dados = new ActiveXObject(fs);
-	//Modo de captura 1: armazenamos tudo numa variável primeiro e depois construimos o objeto
-	/*Aqui capturamos os valores nos inputs da página de cadastro
- 	let senha = document.getElementById('senha').value
-    let nome = document.getElementById('nome').value
-   	let email = document.getElementById('email').value
-   	let telefone = document.getElementById('telefone').value
-   	Agora precisa armazenar tudo isso...*/
-
-    //Modo de captura 2: construimos o objeto direto com a leitura dos inputs sem passar por uma variável primeiro
-    var novo_usuario = new Usuario(document.getElementById('senha').value,document.getElementById('nome').value,document.getElementById('email').value,document.getElementById('telefone').value);
-    let arquivo = dados.OpenTextFile('registro_usuarios.txt', 1, true);//Aqui abrimos o arquivo
-  	EscreverNoArquivo(arquivo,novo_usuario)
-	console.log(novo_usuario.nome+' criou uma conta com a senha '+novo_usuario.senha+', com o e-mail '+novo_usuario.email+' e com o telefone'+novo_usuario.telefone)
-   	console.log('O sistema gerou para ele o id:',novo_usuario.id_usuario)
-   	if(window.confirm("Você está cadastrado, quer fazer login?")){
-   		ir_para_pagina('login.html')
-   	}
-}
-
-
